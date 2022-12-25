0ed2bfc7a8c78a9abfd4ef40d61f0bf8bbeba577
IlyaFedorov7 <fidson5844@>
2022-12-25 14:51:46 +0300

    a

--- task6/task6.py
+++ task6/task6.py
@@ -1,50 +1,43 @@
 def task(csvString):
-
   from io import StringIO
   import csv
   import numpy as np
   import json
 
-  filename = StringIO(csvString)
-  R = csv.reader(filename, delimiter=',')
+  file = StringIO(csvString)
+  r = csv.reader(file, delimiter=',')
   output = []
-  for row in R:
+  for row in r:
     output.append(row)
-
-  def get_table(out, col):
-    table=np.zeros(np.array(out).shape)
+  output=np.array(output).T
+  def get_table(output, col):
+    table=np.zeros(np.array(output).shape)
     for i in range(table.shape[1]):
       for j in range(table.shape[0]):
-        if (out[i][col]<out[j][col]):
+        if (output[i][col]<output[j][col]):
           table[i,j]=1
-        elif (out[i][col]==out[j][col]):
+        elif (output[i][col]==output[j][col]):
           table[i,j]=0.5
         else:
           table[i,j]=0
     return table
-
-  tbls=[]
-
+  tables=[]
   for i in range(len(output)):
-    tbls.append(get_table(output,i))
+    tables.append(get_table(output,i))
   X=np.zeros((len(output),len(output)))
-
   for i in range(len(output)):
-    X+=tbls[i]
+    X+=tables[i]
   X=X/len(output)
   k0=np.array([1/len(output)]*len(output))
   eps=0.001
   Y=X.dot(k0)
   lya1=(np.ones(len(output))).dot(Y)
   k1=1/lya1*Y
-
   while (abs(max(k1-k0))>=eps):
     k0=k1
     Y=X.dot(k0)
     lya1=(np.ones(len(output))).dot(Y)
     k1=1/lya1*Y
-
   for i in range(len(k1)):
     k1[i]=round(k1[i],3)
-
   return(json.dumps(k1.tolist()))
